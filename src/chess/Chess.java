183199fd5af76e6a90906f1c95925894629db07c
unknown <natalina@Alex-PC.(none)>
2012-06-28 15:54:23 -0400

    Beta v1.2.2 -Added a binary flag board to determine which pieces are where -Displays binary flag board to make future coding and debugging easier

--- src/chess/Chess.java
+++ src/chess/Chess.java
@@ -13,11 +13,34 @@ import java.text.*;
 import java.util.concurrent.*;
 
 /**
- *
- * @author
- * Coded By: Alex Craig, Mark Aronin
+ * Coded By: Alex Craig
+ * Debugged By : Alex Craig, Mark Aronin
  * Images By: abener
  */
+
+/**
+ * Binary Board Legend
+ * 
+ * 0000 = 0 = blank
+ * 
+ * 0001 = 1 = wp
+ * 0011 = 3 = wr
+ * 0111 = 7 = wki
+ * 0101 = 5 = wq
+ * 0100 = 4 = wb
+ * 0110 = 6 = wkn
+ * 
+ * 1001 = 9 = bp
+ * 1011 = 12 = br
+ * 1111 = 15 = bki
+ * 1101 = 13 = bq
+ * 1100 = 12 = bb
+ * 1110 = 14 = bkn
+ * 
+ * 0 = white's turn
+ * 1 = black's turn
+ */
+
 public class Chess {
 
     private JFrame frame;
@@ -26,13 +49,16 @@ public class Chess {
     private Color[] squareColors = {Color.white, Color.LIGHT_GRAY};
     private int movingFlag;
     private int selectedFlag;
+    private int turnFlag;
     private JLabel movingPiece;
     private int[] previousSquare = {0, 0};
     private int[] previousHoverSquare = {0, 0};
+    private int[][] binaryBoard;
     
     public Chess() {
         movingPiece = new JLabel();
-        movingFlag=0;
+        movingFlag = 0;
+        turnFlag = 0;
         createBoard();
         addListeners();
     }
@@ -43,7 +69,8 @@ public class Chess {
         frame = new JFrame();
         squares = new JPanel[8][8];
         squareLabels = new JLabel[8][8];
-             
+        binaryBoard = new int[8][8];
+        
         frame.setTitle("Chess Game");
         frame.setSize(800, 800);
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
@@ -53,17 +80,20 @@ public class Chess {
         frame.setLocation((dim.width-(frame.getSize().width))/2, (dim.height-(frame.getSize().height))/2);
         
         for (int j=0; j<8; j++) {         
-            for (int i=0; i<8; i++) {
+            for (int i=0; i<8; i++) {            
                 squares[j][i] = new JPanel();
-                squareLabels[j][i] = new JLabel();               
+                squareLabels[j][i] = new JLabel();
                 squares[j][i].setBackground(squareColors[(i+j)%2]);
                 squares[j][i].setBorder(BorderFactory.createLineBorder(Color.black));               
                 frame.add(squares[j][i]);
                 squares[j][i].add(squareLabels[j][i]);
                 squareLabels[j][i].setIcon(loadPiece(j, i));
+                
+                //TEMP binaryBoard DISPLAY
+                System.out.print(binaryBoard[j][i]+" ");
             }
+            System.out.print("\n");
         }
-   
     }
     
     public ImageIcon loadPiece(int row, int column) {
@@ -72,55 +102,68 @@ public class Chess {
         //Black Pieces
         if (row==0 && (column==0 || column==7)) {
             piece = new ImageIcon(getClass().getResource("/pieces/br.png"));
+            binaryBoard[row][column] = 11;
         }
         
         else if (row==0 && (column==1 || column==6)) {
             piece = new ImageIcon(getClass().getResource("/pieces/bkn.png"));
+            binaryBoard[row][column] = 14;
         }
         
         else if (row==0 && (column==2 || column==5)) {
             piece = new ImageIcon(getClass().getResource("/pieces/bb.png"));
+            binaryBoard[row][column] = 12;
         }
         
         else if (row==0 && column==3) {
             piece = new ImageIcon(getClass().getResource("/pieces/bq.png"));
+            binaryBoard[row][column] = 13;
         }
         
         else if (row==0 && column==4) {
             piece = new ImageIcon(getClass().getResource("/pieces/bki.png"));
+            binaryBoard[row][column] = 15;
         }
         
         else if (row==1) {
             piece = new ImageIcon(getClass().getResource("/pieces/bp.png"));
+            binaryBoard[row][column] = 9;
         }
         
         //White Pieces
         else if (row==6) {
             piece = new ImageIcon(getClass().getResource("/pieces/wp.png"));
+            binaryBoard[row][column] = 1;
         }
         
         else if (row==7 && (column==0 || column==7)) {
             piece = new ImageIcon(getClass().getResource("/pieces/wr.png"));
+            binaryBoard[row][column] = 3;
         }
         
         else if (row==7 && (column==1 || column==6)) {
             piece = new ImageIcon(getClass().getResource("/pieces/wkn.png"));
+            binaryBoard[row][column] = 6;
         }
         
         else if (row==7 && (column==2 || column==5)) {
             piece = new ImageIcon(getClass().getResource("/pieces/wb.png"));
+            binaryBoard[row][column] = 4;
         }
         
         else if (row==7 && column==3) {
             piece = new ImageIcon(getClass().getResource("/pieces/wq.png"));
+            binaryBoard[row][column] = 5;
         }
         
         else if (row==7 && column==4) {
             piece = new ImageIcon(getClass().getResource("/pieces/wki.png"));
+            binaryBoard[row][column] = 7;
         }
         
         else {
             piece = new ImageIcon(getClass().getResource("/pieces/blank.png"));
+            binaryBoard[row][column] = 0;
         }
         
         return piece;
@@ -154,8 +197,17 @@ public class Chess {
                                             ImageIcon tempIcon = new ImageIcon(getClass().getResource("/pieces/blank.png"));
                                             squareLabels[previousSquare[0]][previousSquare[1]].setIcon(tempIcon);                                      
                                             squares[previousSquare[0]][previousSquare[1]].setBackground(squareColors[(previousSquare[0]+previousSquare[1])%2]);
+                                            binaryBoard[i][j] = binaryBoard[previousSquare[0]][previousSquare[1]];
+                                            binaryBoard[previousSquare[0]][previousSquare[1]] = 0;                                           
                                             selectedFlag--;
                                             movingFlag--;
+                                                    //TEMP binaryBoard DISPLAY
+                                                    for (int b=0; b<8; b++) {         
+                                                    for (int a=0; a<8; a++) {            
+                                                    System.out.print(binaryBoard[b][a]+" ");
+                                                    }
+                                                    System.out.print("\n");
+                                                    }
                                         }
                                         
                                         else {
